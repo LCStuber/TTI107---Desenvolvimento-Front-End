e6a34abdb45f8430dec9b77007800bbd1808ba24
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2022-10-14 11:07:07 -0300

    Aula 18

---
+++ Aula 17 e 18/html node e mongo/front/js/script.js
@@ -0,0 +1,7 @@
+const protocolo = "http://"
+const baseURL = "192.168.231.12"
+const filmesEndPOint = "/filmes"
+
+function obterFilmes() {
+    console.log("teste")
+}
--- Aula 17 e 18/html node e mongo/index.js
+++ Aula 17 e 18/html node e mongo/index.js
@@ -4,7 +4,29 @@ const express = require ('express')
 const app = express()
 app.use(express.json())
 
+let filmes = [
+    {
+    titulo: "Forrest Gump - O Contador de Histórias",
+    sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções."
+    },
+    {
+    titulo: "Um Sonho de Liberdade",
+    sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
+    }
+    ]
+    
 //escutar uma requisição get na URL http://localhost:3000/oi
 app.get("/oi", (req, res) => {res.send('Salve!')})
 
-app.listen(3000, () => console.log("up and running"))
+//endpoint para obter a lista de filmes
+app.get("/filmes", (req, res) => {res.send(filmes)})
+
+app.get("/filmes", (req, res) => {
+    const titulo = req.body.titulo
+    const sinopse = req.body.sinopse
+    const filme = {titulo: titulo, sinopse: sinopse}
+    filmes.push(filme)
+    req.json(filmes)
+})
+
+app.listen(80, () => console.log("up and running"))
