9b14c950c6ce6f76a4e3b4487c8ccf08255a54c1
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2022-11-04 10:56:09 -0300

    Aula 24

--- Aula 23 e 24/html node e mongo/backend.js
+++ Aula 23 e 24/html node e mongo/backend.js
@@ -1,26 +1,27 @@
 // console.log('Hello, NodeJS')
 
-const express = require ('express')
-const cors = require ('cors')
+const express = require('express')
+const cors = require('cors')
 const app = express()
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 const uniqueValidator = require('mongoose-unique-validator')
 app.use(express.json())
 app.use(cors())
 
-const Filme = mongoose.model ("Filme", mongoose.Schema({
-    titulo: {type: String},
-    sinopse: {type: String}
+const Filme = mongoose.model("Filme", mongoose.Schema({
+    titulo: { type: String },
+    sinopse: { type: String }
 }))
 
 const usuarioSchema = mongoose.Schema(
     {
-        login: {type: String, required: true, unique: true},
-        password: {type: String, required: true}
+        login: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
     }
 )
 usuarioSchema.plugin(uniqueValidator)
-const Usuario = mongoose.model("Usuário",usuarioSchema)
+const Usuario = mongoose.model("Usuário", usuarioSchema)
 
 // Não é mais necessário já que estamos utilizando o banco de dados mongo
 // let filmes = [
@@ -33,13 +34,13 @@ const Usuario = mongoose.model("Usuário",usuarioSchema)
 //     sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
 //     }
 //     ]
-    
+
 //escutar uma requisição get na URL http://localhost:3000/oi
-app.get("/oi", (req, res) => {res.send('Salve!')})
+app.get("/oi", (req, res) => { res.send('Salve!') })
 
 //Essas duas linhas foram criadas por mim (Não relacionado a aula)
-app.get("/", (req, res) => {res.sendFile('/front/index.html', { root: __dirname })})
-app.get("/js/script.js", (req, res) => {res.sendFile('/front/js/script.js', { root: __dirname })})
+app.get("/", (req, res) => { res.sendFile('/front/index.html', { root: __dirname }) })
+app.get("/js/frontend.js", (req, res) => { res.sendFile('/front/js/frontend.js', { root: __dirname }) })
 
 //endpoint para obter a lista de filmes
 app.get("/filmes", async (req, res) => {
@@ -47,40 +48,50 @@ app.get("/filmes", async (req, res) => {
     res.json(filmes)
 })
 
-async function conectarMongo () {
+async function conectarMongo() {
     await mongoose.connect("mongodb+srv://<username>>:<password>>@lcstuber.bvgn6ah.mongodb.net/?retryWrites=true&w=majority")
 }
 
 //Essas quatro linhas foram criadas por mim (Não relacionado a aula)
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.type('text/html');
     res.status(404).send("<img src='https://http.cat/404.png' style='height: 95vh; display: block; margin-left: auto; margin-right: auto;'>");
-    });
+});
 
 app.post("/filmes", async (req, res) => {
     const titulo = req.body.titulo
     const sinopse = req.body.sinopse
-    const filme = new Filme({titulo: titulo, sinopse: sinopse})
+    const filme = new Filme({ titulo: titulo, sinopse: sinopse })
     await filme.save()
     const filmes = await Filme.find()
     res.json(filmes)
 })
 
-app.post("/signup", async (req,res) => {
-    const login = req.body.login
-    const password = req.body.password
-    const usuario = new Usuario ({login: login, password: password})
-    const respMongo = await usuario.save()
-    console.log(respMongo)
-    res.end()
+app.post("/signup", async (req, res) => {
+    try {
+        const login = req.body.login
+        const password = req.body.password
+        const criptografada = await bcrypt.hash(password, 10)
+        const usuario = new Usuario({ login: login, password: criptografada })
+        const respMongo = await usuario.save()
+        console.log(respMongo)
+        res.status(201).end()
+    }
+    catch (e) {
+        console.log(e)
+        res.status(409).end()
+    }
+})
+
+app.post("/login", async (req, res) => {
 })
 
 app.listen(3000, () => {
-    try{
+    try {
         conectarMongo()
         console.log("up and running")
     }
-    catch (e){
-        console.log ("Erro: ", e)
+    catch (e) {
+        console.log("Erro: ", e)
     }
 })
